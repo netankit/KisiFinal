1add8505c7a87a1a930f8b71fd2c1384fe88bd34
thoermann <thomas@hoermi.com>
2014-01-27 19:57:14 +0100

    Expanded Notification Added

--- src/de/kisi/android/notifications/NotificationManager.java
+++ src/de/kisi/android/notifications/NotificationManager.java
@@ -1,19 +1,19 @@
 package de.kisi.android.notifications;
 
+import java.util.LinkedList;
 import java.util.List;
 
-import android.app.Activity;
-import android.app.Notification; 
+import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.RemoteViews;
 import de.kisi.android.KisiMain;
 import de.kisi.android.R;
 import de.kisi.android.api.KisiAPI;
@@ -26,33 +26,123 @@ public class NotificationManager extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		KisiAPI kisiAPI = KisiAPI.getInstance();
 		if (kisiAPI.getUser() != null) {
-		
-			Log.i("NotificationManager", "onReceive(" + intent.getAction() + ")");
-			Bundle extras = intent.getExtras();
+			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN){
+				
+				Bundle extras = intent.getExtras();
+				int placeId = extras.getInt("Place", -1);
+				String type = extras.getString("Type");
+				if (type.equals("Enter")) 
+					addPlace(KisiAPI.getInstance().getPlaceById(placeId));
+				else
+					removePlace(KisiAPI.getInstance().getPlaceById(placeId),context);
+				showNotifications(context);
+				showNotifications(context);
+				
+			}else{
+				// For Android 4.0
+				Bundle extras = intent.getExtras();
+				
+				int placeId = extras.getInt("Place", -1);
+				String type = extras.getString("Type");
+				
+				Place place = kisiAPI.getPlaceById(placeId);
 			
-			int placeId = extras.getInt("Place", -1);
-			String type = extras.getString("Type");
-			
-			Place place = kisiAPI.getPlaceById(placeId);
-			
-			if (type.equals("Enter")) {
-				List<Lock> locks = place.getLocks();
+				if (type.equals("Enter")) {
+					List<Lock> locks = place.getLocks();
 
-				for(Lock l: locks){
-					showNotification(context, l, place);
+					for(Lock l: locks){
+						showNotification(context, l, place);
+					}
+
+				} else if (type.equals("Exit")) {
+					removeNotifications(context, place);
 				}
-
-			} else if (type.equals("Exit")) {
-				removeNotifications(context, place);
 			}
+		}
+	}
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	public static void showNotifications(Context context){
+		android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.cancelAll();
+			if(places.size()==0){
+				mNotificationManager.cancelAll();
+				return;
+			}else{
+				for(NotificationPlace place : places){
+				NotificationCompat.Builder nb = new NotificationCompat.Builder(context);
+				nb.setSmallIcon(R.drawable.notification_icon)
+				.setContentTitle("KISI")
+			    .setContentText(place.name)
+			    .setDefaults(Notification.DEFAULT_ALL)
+				.setWhen(0);
+				Intent intent = new Intent(context, KisiMain.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				intent.putExtra("Type", "unlock");
+				intent.putExtra("Place", place.id);
+				intent.putExtra("Lock", -1);
+				PendingIntent pIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+				nb.setContentIntent(pIntent);
+				RemoteViews contentView = new RemoteViews(context.getPackageName(), R.layout.notification_view);
+				contentView.setTextViewText(R.id.place, place.name);
+				for(NotificationLock lock : place.locks){
+					RemoteViews button;
+					button = new RemoteViews(context.getPackageName(), R.layout.notification_item);
+					button.setTextViewText(R.id.unlockButton, lock.name);
+					intent = new Intent(context, KisiMain.class);
+					intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					intent.putExtra("Type", "unlock");
+					intent.putExtra("Place", place.id);
+					intent.putExtra("Lock", lock.id);
+					PendingIntent pendingIntent = PendingIntent.getActivity(context, lock.id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+					button.setOnClickPendingIntent(R.id.unlockButton, pendingIntent);
+					contentView.addView(R.id.widget2, button);
+				}
+				Notification notification = nb.build();
+				notification.bigContentView = contentView;
+				mNotificationManager.notify(place.id, notification);
+
+				}
 
 		}
 	}
 
+	private static LinkedList<NotificationPlace> places = new LinkedList<NotificationPlace>();
+
+	private static void addPlace(Place place){
+		if(place == null)
+			return;
+		for(NotificationPlace p:places){
+			if(p.id == place.getId()){
+				places.remove(p);
+				break;
+			}
+		}
+		NotificationPlace newPlace = new NotificationPlace(place.getId(),place.getName());
+		places.add(newPlace);
+		for(Lock lock : place.getLocks()){
+			newPlace.locks.add(new NotificationLock(lock.getId(),lock.getName()));
+		}
+	}
+	
+	private static void removePlace(Place place, Context context){
+		if(place == null)
+			return;
+		for(NotificationPlace p:places){
+			if(p.id == place.getId()){
+				android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+				mNotificationManager.cancel(p.id);
+				places.remove(p);
+				return;
+			}
+		}
+	}
+	
+	
+	
 	public static void removeNotifications(Context context, Place place) {
 
 		android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) context
-				.getSystemService(Activity.NOTIFICATION_SERVICE);
+				.getSystemService(Context.NOTIFICATION_SERVICE);
 		List<Lock> locks = place.getLocks();
 		for (Lock lock : locks) {
 			int id = (lock.getId());
@@ -66,12 +156,11 @@ public class NotificationManager extends BroadcastReceiver {
 
 		NotificationCompat.Builder nc = new NotificationCompat.Builder(c);
 		nc.setSmallIcon(R.drawable.notification_icon);
-		Bitmap bitmap = BitmapFactory.decodeResource(c.getResources(), R.drawable.notification_icon);
-		nc.setLargeIcon(bitmap);
 		nc.setContentText("Touch to Unlock");
 		nc.setContentTitle(lock.getName() + " - " + place.getName());
 		nc.setDefaults(Notification.DEFAULT_ALL);
-		android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) c.getSystemService(Activity.NOTIFICATION_SERVICE);
+		nc.setWhen(0);
+		android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) c.getSystemService(Context.NOTIFICATION_SERVICE);
 		Intent intent = new Intent(c, KisiMain.class);
 		intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		intent.putExtra("Type", "unlock");
@@ -87,7 +176,7 @@ public class NotificationManager extends BroadcastReceiver {
 	}
 	
 	public static void removeAllNotifications(Context context) {
-		android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) context.getSystemService(Activity.NOTIFICATION_SERVICE);
+		android.app.NotificationManager mNotificationManager = (android.app.NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		mNotificationManager.cancelAll();
 	}
 }
