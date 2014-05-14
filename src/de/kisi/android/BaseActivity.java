0d230214dee50c5a52f8469a78defb67f828d07d
florianschaule <fs@kisi.de>
2014-05-14 17:17:41 +0200

    fixed bug that app crashes on devices without BLE

--- src/de/kisi/android/BaseActivity.java
+++ src/de/kisi/android/BaseActivity.java
@@ -13,6 +13,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
@@ -78,11 +79,18 @@ public class BaseActivity extends FragmentActivity implements VersionCheckCallba
 			wifiEnabled = false;
 		}
 		
-		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if(!mBluetoothAdapter.isEnabled()) {
-			bluetoothEnabled = false;
+		//check if this device supports BLE
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2 && 
+				KisiApplication.getInstance().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+			BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			if(!mBluetoothAdapter.isEnabled()) {
+				bluetoothEnabled = false;
+			}
 		}
-		
+		else{ // dont ask for bluetooth if device does not support BLE
+			bluetoothEnabled = true;
+		}
+			
 		//check if there is a need to enable anything
 		if(locationEnabled && wifiEnabled && bluetoothEnabled) {
 			return;
