27ea4a1bfb03d88f868408c16674ed456929e0e8
Mustafa Tok <me@mustafatok.com>
2014-06-05 20:41:40 +0200

    Added error checking.

--- src/de/kisi/android/account/RegisterActivity.java
+++ src/de/kisi/android/account/RegisterActivity.java
@@ -1,5 +1,8 @@
 package de.kisi.android.account;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import de.kisi.android.R;
 import de.kisi.android.api.KisiAPI;
 import de.kisi.android.api.RegisterCallback;
@@ -10,11 +13,12 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegisterActivity extends Activity implements OnClickListener, RegisterCallback{
 	
-	private EditText userNameField;
+	private EditText emailField;
 	private EditText passwordField;
 	private EditText passwordConfirmField;
 	private CheckBox agreedTermsField;
@@ -23,7 +27,7 @@ public class RegisterActivity extends Activity implements OnClickListener, Regis
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register_activity);
 		
-		userNameField = (EditText) findViewById(R.id.email);
+		emailField = (EditText) findViewById(R.id.email);
 		passwordField = (EditText) findViewById(R.id.password);
 		passwordConfirmField = (EditText) findViewById(R.id.passwordConfirm);
 		agreedTermsField = (CheckBox) findViewById(R.id.termsCondition);
@@ -32,30 +36,11 @@ public class RegisterActivity extends Activity implements OnClickListener, Regis
 		registerButton.setOnClickListener(this);
 		
 	}
-
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.registerButton:
-			String username = userNameField.getText().toString();
-			String password = passwordField.getText().toString();
-			String passwordConfirm = passwordConfirmField.getText().toString();
-			boolean termsAgreed = agreedTermsField.isChecked();
-			
-			if(!termsAgreed){
-				Toast.makeText(getApplicationContext(), R.string.terms_and_conditions_not_agreed, Toast.LENGTH_SHORT).show();
-				return;
-			}
-			if(username.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()){
-				Toast.makeText(getApplicationContext(), R.string.fields_cannot_be_empty, Toast.LENGTH_SHORT).show();
-				return;
-			}
-			if(!password.equals(passwordConfirm)){
-				Toast.makeText(getApplicationContext(), R.string.passwords_not_match, Toast.LENGTH_SHORT).show();
-				return;
-			}
-			KisiAPI.getInstance().register(username, password, termsAgreed, this);
-			
+			register();
 			break;
 		default:
 			break;
@@ -73,5 +58,50 @@ public class RegisterActivity extends Activity implements OnClickListener, Regis
 		Toast.makeText(getBaseContext(), errormessage, Toast.LENGTH_SHORT).show();
 	}
 
+	private void focusOnField(TextView field, String error){
+		field.setError(error);			
+	}
+	private boolean validateEmail(String email) {
+		Pattern pattern;
+		Matcher matcher;
+		String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+		pattern = Pattern.compile(EMAIL_PATTERN);
+		matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	private void register(){
+		String email = emailField.getText().toString();
+		String password = passwordField.getText().toString();
+		String passwordConfirm = passwordConfirmField.getText().toString();
+		boolean termsAgreed = agreedTermsField.isChecked();
+		
+		boolean errorFlag = false;
+		
+		if(!validateEmail(email)){
+			focusOnField(emailField, getResources().getString(R.string.email_not_valid));
+			errorFlag = true;
+		}
+		if(!password.equals(passwordConfirm)){
+			focusOnField(passwordField, getResources().getString(R.string.passwords_not_match));
+			focusOnField(passwordConfirmField, getResources().getString(R.string.passwords_not_match));
+			errorFlag = true;
+		}else{
+			if(password.isEmpty() || password.length() < 8){
+				focusOnField(passwordField, getResources().getString(R.string.password_length_error));
+				errorFlag = true;
+			}
+			if(passwordConfirm.isEmpty() || passwordConfirm.length() < 8){
+				focusOnField(passwordConfirmField, getResources().getString(R.string.password_length_error));
+				errorFlag = true;
+			}
+		}
 
+		if(!termsAgreed){
+			focusOnField(agreedTermsField, getResources().getString(R.string.terms_and_conditions_not_agreed));
+			errorFlag = true;
+		}
+		if(!errorFlag){
+			KisiAPI.getInstance().register(email, password, termsAgreed, this);
+		}
+	}
 }
