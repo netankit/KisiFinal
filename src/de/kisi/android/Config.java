bcdfb55a9a3dcbc8aee39cc8b7e86589ce9312ad
Lorenz Stadler <l-o-r-i@gmx.net>
2014-07-15 16:31:38 +0200

    documentation

--- src/de/kisi/android/Config.java
+++ src/de/kisi/android/Config.java
@@ -1,9 +1,13 @@
 package de.kisi.android;
 
+/**
+ * This class contains static configuration values.
+ */
 public class Config {
 
 	// QuickBlox credentials //TODO: Kisi should create a new account, this is only for development (Account of Lorenz Stadler).
-    public static final String APP_ID = "11552";
+    // Copy these value from Quickblox administration site
+	public static final String APP_ID = "11552";
     public static final String AUTH_KEY = "FjubH8XC7tYHLJB";
     public static final String AUTH_SECRET = "PvYVxSDkX9X9tTL";
     public static final String USER_PASSWORD = "64d9bpMC"; //password for temporary users
