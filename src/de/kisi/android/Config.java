7fb696cc595737205979162329c27ceedf5284f2
Lorenz Stadler <l-o-r-i@gmx.net>
2014-07-15 12:13:12 +0200

    VirtualIntercom Bugfix

--- src/de/kisi/android/Config.java
+++ src/de/kisi/android/Config.java
@@ -2,13 +2,10 @@ package de.kisi.android;
 
 public class Config {
 
-	// QuickBlox credentials
-//    public static final String APP_ID = "11463"; //Mustafas account
-//    public static final String AUTH_KEY = "ZFx57rhKCmvVnCg"; //Mustafas account
-//    public static final String AUTH_SECRET = "NSUBw8n5XEzbmKe"; //Mustafas account
-    public static final String APP_ID = "11552"; //Lorenz account
-    public static final String AUTH_KEY = "FjubH8XC7tYHLJB"; //Lorenz account
-    public static final String AUTH_SECRET = "PvYVxSDkX9X9tTL"; //Lorenz account
+	// QuickBlox credentials //TODO: Kisi should create a new account, this is only for development (Account of Lorenz Stadler).
+    public static final String APP_ID = "11552";
+    public static final String AUTH_KEY = "FjubH8XC7tYHLJB";
+    public static final String AUTH_SECRET = "PvYVxSDkX9X9tTL";
     public static final String USER_PASSWORD = "64d9bpMC"; //password for temporary users
 
     // In GCM, the Sender ID is a project ID that you acquire from the API console
